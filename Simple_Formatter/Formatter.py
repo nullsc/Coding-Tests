b42fe34f8f0a9422dae4e99d25ba2a5d2a22c043
nullsc <53406164+nullsc@users.noreply.github.com>
2020-04-30 21:25:47 +0100

    Create Formatter.py

---
+++ Simple_Formatter/Formatter.py
@@ -0,0 +1,28 @@
+#quick text formatter to remove tabs from a text file
+#eg. q  1% -> q
+#30/04/2020
+#version 1
+
+import sys
+
+print("Simple File Formatter")
+
+txtFile = "test.txt"
+outputFile = "output.txt"
+DELIMETER = '\t'
+
+
+print(f"Opening: {txtFile}")
+print(f"Writing to: {outputFile}")
+
+with open(txtFile, 'rt') as file:
+    for line in file:
+        #print(line)
+        newLine = line.split(DELIMETER) #split the string
+        print(newLine[0].rstrip())
+        with open(outputFile, 'at') as outFile:
+            outFile.write(newLine[0].rstrip() + "\n")
+file.close()
+outFile.close()
+
+print("Complete")
