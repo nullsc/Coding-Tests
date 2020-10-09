d69cb0bef5e4e844cda9a4575890a84b2bffd5af
nullsc <53406164+nullsc@users.noreply.github.com>
2020-10-09 23:48:33 +0100

    Update IMDB_Scraper.py

--- IMDB Crawler/IMDB_Scraper.py
+++ IMDB Crawler/IMDB_Scraper.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 
-print("Starting")
+print("IMDB Scraper Starting")
 
 response = requests.get(url,
                         headers={'User-Agent': user_agent}
