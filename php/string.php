4078fa1e8919fe129362509d1071fd3967673efb
nullsc <53406164+nullsc@users.noreply.github.com>
2020-10-28 20:54:36 +0000

    Update string.php

--- php/string.php
+++ php/string.php
@@ -1,6 +1,6 @@
 <?php
-//test
-$test = '  Hello there "John" ';
+
+$test = '  Hello there "John"> ';
 $test = trim($test);
 $test = htmlentities($test);
 echo '<h3>'.$test.'</h3>';
@@ -10,8 +10,9 @@ function sanitize($str){
 	$str = htmlentities($str, ENT_QUOTES);
 	return $str;
 }
+
 $title = " harry potter & the dragon";
-$id = "xw-324"; //2 chars - digits
+$id = "xw-324";
 
 $parts = explode('-', $id);
 echo '<p>Title: '.ucwords(sanitize($title)).'</p>';
@@ -20,4 +21,28 @@ echo '<p>ID: '.$parts[0].'#'.$parts[1].'</p>';
 if(ctype_alpha($parts[0]) && ctype_digit($parts[1])){
 	echo '<p>The ID is valid!</p>';
 }
+
+function times($number){
+	return $number * 10;
+}
+
+function make_heading($string, $number){
+	if($number > 0 && $number < 7){
+		$string = trim($string);
+		if($number == 1 || $number == 2){
+			$string = ucwords($string);
+		}
+		else{
+			$string = ucfirst($string);
+		}
+		
+		$header = "<h$number>$string</h$number>";
+		return $header;
+	}
+	else
+		return $string;
+}
+
+echo make_heading('test heading', 6);
+
 ?>
