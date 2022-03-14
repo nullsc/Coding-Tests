5b426df0e5d63692f52c3f06c16a091b26cab268
nullsc <53406164+nullsc@users.noreply.github.com>
2022-03-14 01:28:38 +0000

    Update test2.java

--- Java/test2.java
+++ Java/test2.java
@@ -19,4 +19,15 @@ public class Main
 	    }
 	    return false;
 	}
+	
+	public static boolean palin(String str) {
+	    System.out.println(str);
+	    if (str.length() < 2)
+	        return true;
+	        
+        if(str.charAt(0) == str.charAt(str.length()-1))
+            return palin(str.substring(1, str.length()-1));
+        
+	    return false;
+	}
 }
