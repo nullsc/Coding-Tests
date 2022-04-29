9357b7bd6202a5ba1e4040df4a9746fcda3d66ef
nullsc <53406164+nullsc@users.noreply.github.com>
2022-04-29 19:55:18 +0100

    Update river.java

--- java-2022/may-2021/river.java
+++ java-2022/may-2021/river.java
@@ -4,15 +4,16 @@ public class river extends waterbody
     private waterbody flow;
     
 	public river(String name, waterbody flow) {
+		//System.out.println("Hello World");
 		super(name);
 		this.flow = flow;
-		
 	}
 	
 	public String getName(){
 	    return super.getName();
 	}
 	
+	@Override
 	public boolean hasFresh(){
 	    return true;
 	} 
