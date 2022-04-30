e5890dfc1bdc5c49fd2b6fcf50685028fa395e48
nullsc <53406164+nullsc@users.noreply.github.com>
2022-04-30 15:21:10 +0100

    Update main.java

--- java-2022/may-2021/main.java
+++ java-2022/may-2021/main.java
@@ -14,10 +14,26 @@ public class Main
         System.out.println(r1.hasFresh());
         System.out.println(sea.hasFresh());
 		
-		       List<String> xs = Arrays.asList(new String[]{"Hello", "World", "!"});
+	List<String> xs = Arrays.asList(new String[]{"Hello", "World", "!"});
         List<Integer> ys = Arrays.asList(new Integer[]{1, 2, 3});
         int d = (new LengthDistancer()).totalDistance(xs, ys);
         System.out.println(d); // prints 9
 		
+		System.out.println(lastValue(new Object[]{new Object(), 1.5, 3, "42"}));
+	}
+	
+	public static double lastValue(Object[] arr){
+	    double index = -1;
+	    
+	    for(int i=arr.length-1; i>=0; i--){
+	        if(arr[i] instanceof Number){
+    	        Number x = (Number)arr[i];
+    	        double y = x.doubleValue();
+    	        //System.out.println(y);
+    	       index = y;
+    	       break;
+	        }
+	    }
+	    return index;
 	}
 }
