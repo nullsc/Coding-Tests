775c7cc3a77fb6a9f130504af349968be3fc05a3
nullsc <53406164+nullsc@users.noreply.github.com>
2022-05-10 23:09:43 +0100

    Update main.java

--- java-2022/mock/main.java
+++ java-2022/mock/main.java
@@ -1,1 +1,24 @@
 //
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		StringHalver halver = new StringHalver();
+        System.out.println( halver.process("Birkbeck") );
+        System.out.println( halver.process("BBBKK") );
+	}
+	/* works
+	public static double printAllReports(Person[] persons){
+	    if(persons == null)
+	        throw new IllegalArgumentException();
+	        
+	   for(Person p: persons){
+	       if(p instanceof HasReports){ // apply to interface to see if person is a manager
+	           HasReports h = (HasReports)p; // cast
+	            h.PrintReports();
+	       }
+	   }
+	   return 0;
+	}*/
+}
