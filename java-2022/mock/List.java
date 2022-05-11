8f7fa4d76c4d0ea3fdc14408fdc54633759e82c3
nullsc <53406164+nullsc@users.noreply.github.com>
2022-05-11 21:57:26 +0100

    Update List.java

--- java-2022/mock/List.java
+++ java-2022/mock/List.java
@@ -3,11 +3,19 @@ import java.util.ArrayList;
 public class List<T> {
     private Element<T> head;
     
-    List(ArrayList<? extends T> ls){ // linkedlist
+    List(ArrayList<? extends T> ls){
         for(int i=ls.size()-1; i>=0; i--){ // traverse backwards
             head = new Element(ls.get(i), head);
             
         }
         
+    }
+    
+    public ArrayList<T> toArrayList(){
+        ArrayList res = new ArrayList<T>();
+        if(head != null){
+            head.copyAr(res);
+        }
+        return res;
     }
 }
