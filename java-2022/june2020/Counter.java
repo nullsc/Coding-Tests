e6295cb20461c726b92b1640818d21121acdfa19
nullsc <53406164+nullsc@users.noreply.github.com>
2022-05-12 23:28:26 +0100

    Create Counter.java

---
+++ java-2022/june2020/Counter.java
@@ -0,0 +1,19 @@
+public class Counter {
+    private int count;
+    
+    public void reset() {
+        count = 0;
+    }
+    
+    public void click() {
+        count++;
+    }
+    
+    public int getCount() {
+        return count;
+    }
+    
+    public String toString() {
+        return "the count is " + count;
+    }
+}
