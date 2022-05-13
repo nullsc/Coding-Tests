1d29e2d43b48528bcfbbf3193f1ce12b95d96551
nullsc <53406164+nullsc@users.noreply.github.com>
2022-05-13 20:25:52 +0100

    Create WeirdInputException.java

---
+++ java-2022/june2020/WeirdInputException.java
@@ -0,0 +1,12 @@
+
+public class WeirdInputException extends RuntimeException { // unchecked exception
+    
+    public WeirdInputException(){
+        super();
+    }
+    
+    public WeirdInputException(String s){
+        super(s);
+    }
+
+}
