f46e3a90da2e970039560faff29a995a7fac2b53
nullsc <53406164+nullsc@users.noreply.github.com>
2022-05-14 17:29:01 +0100

    Update main.java

--- java-2022/june2020/main.java
+++ java-2022/june2020/main.java
@@ -1,4 +1,6 @@
-//
+/******************************************************************************
+
+*******************************************************************************/
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -31,24 +33,31 @@ public class Main
         int n = f.countAccepted(a);
         System.out.println("Accepted: " + n);
         
-        getInput();
+        //getInput();
+        Tree t = Tree.create(2);
+        
+        int[] values = { 1, 2, 3, 4, 5, 6 };
+        boolean allCopied = t.fill(values);
+        
+        System.out.println(toPalindromeRecursive("test"));
+        //System.out.println(toPalindromeRecursive2("test", 3));
 
 	}
 	
-  // keep taking int inputs, prints of number is > than the previous
+	
 	public static void getInput(){
 	    int first = 0;
 	    int second = 0;
 	    
     	Scanner sc = new Scanner(System.in);  // Create a Scanner object
-    	while(true){ // can use sc.hasNext()
+    	while(true){
             System.out.println("Enter a number");
             if(sc.hasNextInt()){
                 first = sc.nextInt();
                 if(first == 0)
                     return;
                 
-                if(second != 0){ // check if value has changed
+                if(second != 0){
                     if(first > second){
                         System.out.println("Bigger");
                     } else if(first < second){
@@ -65,5 +74,28 @@ public class Main
             }
     	}
 
+	}
+	
+	/* test = testtset*/
+	public static String toPalindromeRecursive(String s){
+	    if (s == null)
+            throw new IllegalArgumentException();
+            
+	    if(s.length() == 0)
+	        return "";
+	        
+	   //System.out.println(s);
+	   
+	    return s.charAt(0) + toPalindromeRecursive(s.substring(1)) + s.charAt(0);
+	}
+	
+	public static String toPalindromeRecursive2(String s, int pos){ // not working
+	    if(pos == 0)
+	        return s;
+	        
+	    if(s.length() <= 1)
+	        return s.charAt(0) + s;
+	        
+	    return toPalindromeRecursive2(s, pos-1) + s.charAt(pos);
 	}
 }
