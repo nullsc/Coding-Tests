d6fa4cdf2167dff68cdec0f4790a8c0ef98e08a5
nullsc <53406164+nullsc@users.noreply.github.com>
2022-05-15 17:25:06 +0100

    Create Pair.java

---
+++ java-2022/May-2018/Pair.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Pair<A,B> {
+    private A a;
+    private B b;
+    
+    public Pair(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+    public A getFirst() { return this.a; }
+    public B getSecond() { return this.b; }
+    
+    public String toString() {
+        return "(" + this.a + "," + this.b + ")";
+    }
+    
+
+
+}
