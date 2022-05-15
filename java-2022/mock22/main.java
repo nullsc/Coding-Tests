9dbd34cbff6e6e81b6ee15d6e689145a533f8c63
nullsc <53406164+nullsc@users.noreply.github.com>
2022-05-16 00:55:16 +0100

    Update main.java

--- java-2022/mock22/main.java
+++ java-2022/mock22/main.java
@@ -1,1 +1,48 @@
 //
+/******************************************************************************
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		MyStringComparator cmp = new MyStringComparator();
+		
+		System.out.println(cmp.compare("bb", null));
+        System.out.println(cmp.compare("bb", "a"));
+        System.out.println(cmp.compare("bb", "aa"));
+        System.out.println(cmp.compare("bb", "bb"));
+        System.out.println(cmp.compare(null, null)); 
+        System.out.println(cmp.compare("bb", "aaa"));
+        System.out.println(cmp.compare("bb", "ccc"));
+        System.out.println(cmp.compare(null, ""));
+        
+        /* calculate the sum or all ints in arg */
+        int total = 0;
+        for(String i: args){
+            Scanner sc = new Scanner(i);
+            while(sc.hasNext()){
+                if(sc.hasNextInt()){
+                    total += sc.nextInt();
+                } else {
+                    sc.next();
+                }
+            }
+            
+        }
+        System.out.println("Total: " + total);
+      
+	}
+	
+	public static int sumRecursive(int[] numbers){ // not finished
+	    if(numbers.length == 0)
+	        return;
+	    
+	    
+	    return sumRecursive();
+	    
+	}
+}
